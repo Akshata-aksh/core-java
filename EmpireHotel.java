29068aedc09770c4c00d67c765bebf077e70e6bd
Akshata-aksh <akshatarpujari.xworkz@gmail.com>
2022-09-22 13:08:19 +0530

    committing array files

---
+++ EmpireHotel.java
@@ -0,0 +1,34 @@
+class EmpireHotel
+static String foodMenu[];
+static String address = "Mg Road";
+
+{
+public static void main(String a[])
+{
+String type = "5 Star Hotel";	
+String[] managerNames = {"Akshata", "Lakshmi", "Sangeeta", "Kavya", "Manjula"};
+int noOfManagers = 5;
+String foodMenu[] = {"Sezwaan fried rice", "Gobhi manchoori", "Noodles", "Pizza", "Burger", "Kaaju kurma", "Egg hundi", "Butternon", "Tandoori roti", "Ice Cream" };
+System.out.println("Welcome to EmpireHotel");
+System.out.println("Destination : "+ address);
+System.out.println("Name of List of Managers");
+
+
+   for(int c = 0; c < managerNames.length; c++){
+System.out.println(managerNames[c]);	
+	
+}
+  for(int z = 0; z < foodMenu.length; z++){
+System.out.println(foodMenu[z]);	
+	
+}
+
+
+}
+
+
+
+
+
+}
+
