8cc7df643b08790fb4d5b18f058d57ad3f23d238
Akshata-aksh <akshatarpujari.xworkz@gmail.com>
2022-09-27 15:41:17 +0530

    commiting settergetter files

---
+++ Flower.java
@@ -0,0 +1,23 @@
+class Flower{
+
+static String flowerName;
+static String flowerColor;
+
+static void setFlowerName(String fName){
+flowerName = fName;
+}
+static String getFlowerName(){
+return flowerName ;
+}
+static void setFlowerColor(String fColor){
+flowerColor = fColor;
+	}
+static String getFlowerColor(){
+return flowerColor ;	
+	
+}
+
+}
+
+
+
---
+++ TeaStalls.java
@@ -0,0 +1,40 @@
+class TeaStalls{
+
+static int teastallId ;
+static String name ;
+static String address  ;
+static long contactNumber;
+
+static void setTeastallId(int tId){
+teastallId = tId;
+}
+
+static int getTeastallId(){
+return teastallId;
+}
+
+static void setName(String nm){
+name = nm ;
+
+}
+static String getName(){
+return name;
+
+}
+static void setAddress(String add ){
+address = add ;
+
+}
+static String getAddress(){
+return address;
+
+}
+//static void setContactNumber(long cNumber){
+//contactNumber = cNumber;	
+		
+}
+//static long getContactNumber(){
+//return contactNumber;	
+	
+//}
+//}
