7450432b2957d6f399f8c0ba9a8b48d21e7585bf
Akshata-aksh <akshatarpujari.xworkz@gmail.com>
2022-11-09 22:59:09 +0530

    committing inheritance files

---
+++ festival/src/com/xworkz/festivalapp/Festival.java
@@ -0,0 +1,15 @@
+package com.xworkz.festivalapp;
+
+public class Festival{
+	
+	Festival(){
+	System.out.println("Festival");
+	}
+   String name;
+   int noOfDays;
+  
+  void pooje() {
+	  
+	  System.out.println("Welcome");
+  }
+}
---
+++ festival/src/com/xworkz/festivalapp/FestivalTester.java
@@ -0,0 +1,13 @@
+package com.xworkz.festivalapp;
+
+public class FestivalTester {
+	public static void main(String args[]) {
+		
+	Festival fest = new Festival();
+	fest.name = "Dipavali";
+	fest.noOfDays = 3;
+	System.out.println(fest.name+" "+fest.noOfDays);
+		
+	}
+
+}
