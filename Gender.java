537d7f8932bb65dd6fef5af6194cca402c3aca5b
Akshata-aksh <113760270+Akshata-aksh@users.noreply.github.com>
2022-12-06 18:46:50 +0530

    Add files via upload

---
+++ Gender.java
@@ -0,0 +1,8 @@
+package com.xworkz.hospital.hospitalapp.constant;
+//it is not used to create object,  to create constant value 
+public enum Gender {
+ female,male,transgender, others;
+	Gender(){
+		
+   }
+}
---
+++ WrapperTester.java
@@ -0,0 +1,62 @@
+package com.xworkz.hospital;
+
+import java.util.Scanner;
+
+public class WrapperTester {
+	
+	public static void main(String args[]) {
+		
+		Scanner scanner = new Scanner(System.in) {
+			System.out.println("Enter the age");
+			int age = scanner.nextDouble();
+			
+			
+		}
+		
+	}
+	}
+		
+		
+//		int age = 89;
+		//Boxing
+		
+//		System.out.println(age);
+		
+//		Integer age1 =age;
+		//un-boxing;
+		
+		/*int age2 = age1;
+		
+		long num = 90876886L;
+		
+		Long num1 =num;
+		
+	    float numb = 0.89f;
+	    
+	    Float numb1 = numb;
+	    
+	    char b = 'A';
+	    
+	    Character b1 = b;
+	    
+	    boolean t = true;
+	    
+	    Boolean t1 = t;
+	    
+	    byte val= 64;
+	    
+	    Byte val1 = val;
+	    
+	    double valu = 839892892;
+	    
+	    Double valu1 = valu;
+	    
+	    short value = 743;
+	    
+	    Short value1 = value;
+	    
+	    
+	    
+	}
+
+}*/
