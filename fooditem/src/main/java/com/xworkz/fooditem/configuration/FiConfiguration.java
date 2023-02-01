45e7e7add01f5e068ee1aefbd98c4619e59bb5a5
Akshata-aksh <akshatarpujari.xworkz@gmail.com>
2023-02-01 22:10:13 +0530

    commiting msvfiles

---
+++ fooditem/src/main/java/com/xworkz/fooditem/configuration/FiConfiguration.java
@@ -0,0 +1,14 @@
+package com.xworkz.fooditem.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.xworkz.fooditem")
+public class FiConfiguration {
+	
+	public FiConfiguration() {
+System.out.println("created "+this.getClass().getSimpleName());	
+}
+
+}
