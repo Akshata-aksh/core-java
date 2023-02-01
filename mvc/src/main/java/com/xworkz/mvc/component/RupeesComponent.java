f67a80a42359fd889d83255f97bd0d44c56a01a7
Akshata-aksh <akshatarpujari.xworkz@gmail.com>
2023-02-01 08:10:56 +0530

    commiting msv files

---
+++ mvc/src/main/java/com/xworkz/mvc/component/RupeesComponent.java
@@ -0,0 +1,11 @@
+package com.xworkz.mvc.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RupeesComponent {
+	public RupeesComponent() {
+System.out.println("created " + this.getClass().getSimpleName());
+	}
+
+}
